82e447724e6e0777381dabeb94d0d0215b6bf6b5
Akhil <akhilenderb9@gmail.com>
2023-06-08 16:04:48 +0530

    Added a custom loader component for the api call

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
 import react, { useState } from "react";
 import "./App.css";
 import UserData from "./components/UserData";
-import CircularProgress from "@mui/material/CircularProgress";
+import Loader from "./components/Loading";
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [load,setLoad] = useState(false);
+
   const handleClick = async () => {
+    setLoad(true);
     const response = await fetch("https://reqres.in/api/users?page=1");
     const jsonRes = await response.json();
     const dt = jsonRes.data;
     console.log(dt);
     setUsers(dt);
+    setLoad(false);
   };
   return (
     <>
       <h1>API CALL USING REACT</h1>
       <button onClick={handleClick}>GET USERS</button>
-      {users ? <UserData users={users}/> : <CircularProgress />}
+      {!load && <UserData users={users}/>}
+      {load && <Loader /> }
     </>
   );
 }
